ea35985bc85b29465ff60eeab9d74b62c8c8fb17
BoyIsDino <arsenalimran@hotmail.com>
2016-06-23 19:08:55 +0100

    Update Main.php

--- src/Nick/Main.php
+++ src/Nick/Main.php
@@ -17,11 +17,11 @@ class Main extends PluginBase implements Listener {
    public function onCommand(CommandSender $sender, Command $cmd, $list, Array $args){
     switch($cmd){
             case "nick":
-                     if(count($args == 1)){
+                     if(count($args == 2)){
                        if($args[0] == "set"){
-                    $sender->sendMessage(TF::BOLD . TF::ITALIC . TF::BLUE . "Your nick has now been set to " . TF::RED . $args[0]);
+                    $sender->sendMessage(TF::BOLD . TF::ITALIC . TF::BLUE . "Your nick has now been set to " . TF::RED . $args[1]);
                 if($sender instanceof Player){         
-      $sender->setNameTag(TF::BOLD. TF::BLUE . $args[0]);
+      $sender->setNameTag(TF::BOLD. TF::BLUE . $args[1]);
              if($args[0] == null){
                $sender->sendMessage(TF::RED . "Invalid Nick!");
                }
